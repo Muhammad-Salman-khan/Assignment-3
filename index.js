49cc0dbee99ac1f0aef82521c374b986bf914d85
Muhammad-Salman-khan <alibinkhan465@gmail.com>
2026-02-05 14:16:22 +0500

    Add initial implementation for Set Behavior Investigation task

---
+++ index.js
@@ -0,0 +1,12 @@
+// Section 1: Set & Map (Real Usage)
+// Task 1.1 – Set Behavior Investigation
+// Create a program that:
+// Stores values of diferent types in a  Set
+// Attempts to add duplicate values intentionally
+// Logs the size and contents after each operation
+// Mandatory:
+// Use at least one object and one array as values
+// Modify a stored object and observe the result
+// Task 1.1 – Set Behavior Investigation (Start!)
+
+// Task 1.1 – Set Behavior Investigation (End!)
